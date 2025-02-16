f821062e267366def7b507a8f570af92f0052526
RahulGoud <rampallyrahulgoud@gmail.com>
2025-02-16 19:23:21 +0530

    Updated project with latest changes

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def predict():
     return render_template('predict.html', prediction_result=sentiment)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
